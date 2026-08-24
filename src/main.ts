1deb9b2058c1529b345d9bbc315f094d351fd9ff
ivangemini <ivangemini13@gmail.com>
2026-08-24 22:12:52 +0300

    feat: restore persisted progress before game boot

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import * as Phaser from 'phaser';
 import { createGameConfig } from './game/config';
+import { WebAdapter } from './platform/WebAdapter';
+import { loadGameSave } from './state/save';
 import './style.css';
 
-const game = new Phaser.Game(createGameConfig('game-root'));
+const platform = new WebAdapter();
+await platform.initialize();
+const initialSave = await loadGameSave(platform);
+const game = new Phaser.Game(createGameConfig('game-root', { platform, initialSave }));
 
 if (import.meta.hot) {
   import.meta.hot.dispose(() => game.destroy(true));
